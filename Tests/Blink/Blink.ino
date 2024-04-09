77dd905b5c7bf7c1ab77625bb93214c8bbb3e008
Benedikt_MBP <provers.0-pluses@icloud.com>
2024-04-09 21:48:41 +0000

    added libraries and LED blink folder

--- Tests/Blink/Blink.ino
+++ Tests/Blink/Blink.ino
@@ -4,7 +4,7 @@
 * Auther      : www.freenove.com
 * Modification: 2020/07/11
 **********************************************************************/
-#define LED_BUILTIN  2
+#define LED_BUILTIN 2
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
@@ -13,9 +13,9 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(200);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
   delay(200);                       // wait for a second
 }
 
