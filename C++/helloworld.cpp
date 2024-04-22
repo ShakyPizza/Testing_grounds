224f179d7cb2a54f50c54d251fdf63f07aa2d722
BOP PC <provers.0-pluses@icloud.com>
2024-04-22 10:56:16 +0000

    final version of hello_world

--- C++/helloworld.cpp
+++ C++/helloworld.cpp
@@ -1,6 +1,6 @@
 #include <iostream> // Include the Input/Output stream library
 
 int main() { // Main function where the execution of program begins
-    std::cout << "Hello, World!" << std::endl; // Output "Hello, World!" to the console
+    std::cout << "Prufa prufa!" << std::endl; // Output "Hello, World!" to the console
     return 0; // Return 0 to indicate that the program ended successfully
 }
