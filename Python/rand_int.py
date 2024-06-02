76a8e1876d8d8edc0d123b428cae72537b502c5e
Benedikt_MBP <provers.0-pluses@icloud.com>
2024-06-02 23:12:33 +0000

    added checks for numbers

--- Python/rand_int.py
+++ Python/rand_int.py
@@ -1,4 +1,39 @@
 import random
 
-# # # Generate a random number between 1 and 100.000 # # #
-random_number = random.randint(1, 100000)
+
+def play_round():
+    random_number = random.randint(1, 2)
+    guessed_correctly = False
+    attempts = 0
+    max_score = 10
+
+    while not guessed_correctly and attempts < 10:
+        attempts += 1
+        print("What is you guess? ")
+        guess = int(input())
+
+        if guess < random_number:
+            print("Your guess is too low")
+
+        elif guess > random_number:
+            print("Your guess is too high")
+
+        else:
+            print("Congratulations! you've guessed the correct number!")
+            guessed_correctly = True
+
+    score = max(max_score - attempts + 1, 0)
+
+    return score
+
+total_score = 0 
+
+while True:
+    total_score += play_round()
+
+    play_again = input("Do you want to play again? (yes/no)").strip().lower()
+    if play_again not in ["yes", "y", "ye"]:
+        print(f"Your total score after this round is: {total_score}")
+        break
+
+print(f"Your total score after all rounds is: {total_score}")
